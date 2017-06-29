3f03071c5bcee689f5dbd2e5078193414300f3e7
caluong1 <catherine.lu1496@gmail.com>
2017-06-29 14:38:35 -0700

    Update text_adventure_starter.py

--- Unit1_Foundations/text-adventure/text_adventure_starter.py
+++ Unit1_Foundations/text-adventure/text_adventure_starter.py
@@ -12,8 +12,11 @@ print(start)
 print("Type 'left' to go left or 'right' to go right.")
 user_input = input()
 if user_input == "left":
-    print("You decide to go left and...") # finished the story by writing what happens
+    print("You decide to go left and find...") # finished the story by writing what happens'''
+    ...a lion staring calmly at you.  Great.  Of all things to see in a different hallway, you had to
+    pick the one with the wild animal in it.  
  
 elif user_input == "right":
     print("You choose to go right and ...") # finished the story writing what happens
  
+else print("...what?")
